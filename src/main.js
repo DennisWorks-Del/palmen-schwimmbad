0a617c1bf29244f24d72362a10a53c8fd3f2c20f
Dennis Schott <schott_dennis@gmx.de>
2025-10-06 22:05:46 +0200

    chore: initial project setup (HTML/CSS/JS + docs)

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Palmen-Schwimmbad – JavaScript Basis
+console.log("Palmen-Schwimmbad Website gestartet");
+
+// Mini-Test: heutiges Datum in der Konsole anzeigen
+const today = new Date().toLocaleDateString("de-DE");
+console.log("Heute ist:", today);
